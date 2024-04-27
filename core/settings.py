8fbb1b359dda7ccdad28baeaec8039caece58d0f
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-04-26 23:21:27 -0300

    feat: make migration

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'curso_sistema_erp',
+        'USER': 'root',
+        'PORT': '3306'
     }
 }
 
